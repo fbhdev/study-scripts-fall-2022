98a5f0ad439ae3b7bc1559d2b1fe652cf57cd9fd
FBH514 <kittens-paring0f@icloud.com>
2022-10-10 21:28:59 -0400

    Added Web Services, modules 1-2-3 done

--- src/WebServices.java
+++ src/WebServices.java
@@ -5,12 +5,13 @@ public class WebServices {
 
     private static ModuleOne moduleOne;
     private static ModuleTwo moduleTwo;
+    private static ModuleThree moduleThree;
     private static final ArrayList<Module> modules = new ArrayList<>();
 
     public WebServices() {
         moduleOne = new ModuleOne();
         moduleTwo = new ModuleTwo();
-//        moduleThree = new ModuleThree();
+        moduleThree = new ModuleThree();
     }
 
     static class ModuleOne{
@@ -50,17 +51,42 @@ public class WebServices {
         }
 
         public void questions(){
-            new Question("", "", 1, module);
-            new Question("", "", 2, module);
-            new Question("", "", 3, module);
-            new Question("", "", 4, module);
-            new Question("", "", 5, module);
+            new Question("What language is used to display data and focuses on how data looks?", "HTML", 1, module);
+            new Question("What provides a framework to define markup languages?", "XML", 2, module);
+            new Question("What is used for distinct, but related, purposes in XML?", "CDATA", 3, module);
+            new Question("What are used for providing uniquely named elements and attributes in an XML document. They are defined in a W3C recommendation?", "Namespaces", 4, module);
+            new Question("What is a description of a type of XML document, typically expressed in terms of constraints on the structure and content of documents of that type, above and beyond the basic syntactical constraints imposed by XML itself?", "XML Schema", 5, module);
+            new Question("What uses path expressions to select nodes or node-sets in an XML document?", "xpath", 6, module);
+            new Question("What can respond to any type of request, but are commonly used to extend the applications hosted by web servers?", "Servlets", 7, module);
+            new Question("What is a part of a URL that assigns values to specified parameters?", "Query String", 8, module);
+        }
+    }
+    static class ModuleThree{
+
+        private Module module;
+
+        public ModuleThree(){
+            module = new Module("Web Services", 3);
+            modules.add(module);
+        }
+
+        public void questions(){
+            new Question("What is a software architectural style that defines a set of constraints to be used for creating Web services?", "REST", 1, module);
+            new Question("What refers to the ability of software to easily integrate with other systems in terms of sharing functionality and data?", "interoperability", 2, module);
+            new Question("Name the terms that means how calls can be made independently of one another, and each call contains all of the data necessary to complete itself successfully.", "stateless", 3, module);
+            new Question("Which verb provides a read only access to a resource?", "GET", 4, module);
+            new Question("Which verb is used to create a new resource?", "POST", 5, module);
+            new Question("Which verb is used to remove a resource?", "DELETE", 6, module);
+            new Question("Which verb is used to update an existing resource or create a new resource?", "PUT", 7, module);
+            new Question("What refers to storing the server response in the client itself, so that a client need not make a server request for the same resource again and again?", "Caching", 8, module);
+            new Question("What may be received at the same time which may cause conflicts? ", "Concurrency", 9, module);
         }
     }
 
     public static void release(){
         moduleOne.questions();
         moduleTwo.questions();
+        moduleThree.questions();
     }
 
     public static ArrayList<Question> shuffle(){
